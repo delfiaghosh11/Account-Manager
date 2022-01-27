51c41cc083ffddc89d068ccfaf34311da616f5cb
Delfia <lovemyself.11.jl@gmail.com>
2022-01-27 19:40:57 +0530

    fix: 🐛 for base route landing on dashboard page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,21 +11,21 @@ import "./App.css";
 const App = (): JSX.Element => (
   <Router>
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/Account-Manager">Account Manager</Navbar.Brand>
+      <Navbar.Brand href="/Account-Manager/">Account Manager</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/Account-Manager">Dashboard</Nav.Link>
-          <Nav.Link href="/accounts">Accounts</Nav.Link>
+          <Nav.Link href="/Account-Manager/">Dashboard</Nav.Link>
+          <Nav.Link href="/Account-Manager/accounts">Accounts</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
 
     <Switch>
-      <Route exact path="/Account-Manager">
+      <Route exact path="/Account-Manager/">
         <Dashboard banks={banks} />
       </Route>
-      <Route path="/accounts">
+      <Route path="/Account-Manager/accounts">
         <AccountSummary banks={banks} />
       </Route>
     </Switch>
