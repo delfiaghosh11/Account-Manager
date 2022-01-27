1d49e292c54298fd1ae7aee3245acb54d7ed140c
Delfia <lovemyself.11.jl@gmail.com>
2022-01-27 22:11:34 +0530

    fix: 🐛 for deploying using HashRouter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { Nav, Navbar } from "react-bootstrap";
 
@@ -9,16 +9,18 @@ import { banks } from "./mockData";
 import "./App.css";
 
 const App = (): JSX.Element => (
-  <Router>
+  <Router basename="/">
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href={process.env.PUBLIC_URL + "/"}>
+      <Navbar.Brand as={Link} to="/">
         Account Manager
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href={process.env.PUBLIC_URL + "/"}>Dashboard</Nav.Link>
-          <Nav.Link href={process.env.PUBLIC_URL + "/accounts"}>
+          <Nav.Link as={Link} to="/">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link as={Link} to="/accounts">
             Accounts
           </Nav.Link>
         </Nav>
@@ -26,10 +28,10 @@ const App = (): JSX.Element => (
     </Navbar>
 
     <Switch>
-      <Route exact path={process.env.PUBLIC_URL + "/"}>
+      <Route exact path="/">
         <Dashboard banks={banks} />
       </Route>
-      <Route path={process.env.PUBLIC_URL + "/accounts"}>
+      <Route path="/accounts">
         <AccountSummary banks={banks} />
       </Route>
     </Switch>
